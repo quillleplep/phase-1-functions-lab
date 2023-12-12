2750835894eba1cf862be15441ed80215f1e653a
quill_nichols <leplep00100@gmail.com>
2023-12-12 14:30:34 -0500

    functional family

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function distanceFromHqInBlocks(someValue){
+    return Math.abs(someValue - 42)
+}
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264
+}
+function distanceTravelledInFeet(start, destination){
+    const distance = Math.abs(start - destination) * 264
+    return distance
+}
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination)
+
+    if (distance <= 400) {
+        return 0
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
 // Code your solution in this file!
